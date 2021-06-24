c84f4d5019d8756926ddaf4ed1561e10aafd34f2
Eugenefer <42931695+Eugenefer@users.noreply.github.com>
2021-06-24 16:06:24 +0200

    Update Avvio.java
    Un cambiamento importante

--- MiaPrimaCondivisione/src/Avvio.java
+++ MiaPrimaCondivisione/src/Avvio.java
@@ -2,10 +2,10 @@
 public class Avvio {
 
 	//Heyla!!! Saluti da Mirko!!!
-	// Ciaoooooooooooooooooo!!! :D:D:D:D
+	// Ciaoooooooooooooooooo!!! >:D>:D>:D>:D
 	public static void main(String[] args) {
-		System.out.println("ciao a tutti!!");
+		System.out.println("ciao a tutto il pubblico femminile!!");
 		System.out.println("Ciao da leonardo");
 	}
-	//Ciao da fabiano
+	//Ciao da Faby
 }
